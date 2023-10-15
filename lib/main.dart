95c0c988c25bd3312e62e55a39fc2ae11e269ca2
Jon Ziebell <ziebelje@gmail.com>
2023-10-15 15:47:48 -0400

    Updated for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
+
   runApp(
     const MaterialApp(
       home: BeestatWidget(),
@@ -22,7 +24,20 @@ class BeestatWidgetState extends State<BeestatWidget> {
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
+
+    // bluegray_light
+    final color = Color.fromRGBO(55, 71, 79, 1);
+
+    // Set the top status bar color
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: color,
+        systemNavigationBarColor: color,
+        systemNavigationBarDividerColor: color
+      )
+    );
+    
+    this.controller = WebViewController()
       ..loadRequest(Uri.parse('https://app.beestat.io'))
       ..setJavaScriptMode(JavaScriptMode.unrestricted);
   }
@@ -30,9 +45,10 @@ class BeestatWidgetState extends State<BeestatWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewWidget(
-        controller: controller
-      )
-    );
+        backgroundColor: Color.fromRGBO(55, 71, 79, 1),
+        body: WebViewWidget(
+          controller: controller
+        )
+      );
   }
 }
